2fc610854d626e4ce0e976a402d8c9536db61dae
shchegolenkov <denis.amorales@ya.ru>
2023-01-07 10:17:28 +0300

    feat: reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let reversed = '';
+  n = n.toString();
+
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] === '-') {
+      reversed = `${reversed}`;
+    } else {
+      reversed = `${n[i]}${reversed}`;
+    }
+  }
+
+  return reversed;
 }
